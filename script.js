623483e432314c5c6cee252568773ef07b52befb
badrudoza <106438583+badrudoza@users.noreply.github.com>
2025-07-09 04:29:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let btn=document.getElementById("submit");
+btn.addEventListener("click",function(e){
+	e.preventDefault();
+	let input=document.querySelectorAll("form input");
+	let fisrname=input[0].value;
+	let lastname=input[1].value;
+	let phno=input[2].value;
+	let email=input[3].value;
+	alert(`First Name: ${fisrname} Last Name: ${lastname} Phone Number: ${phno} Email ID: ${email}`);
+});
